f3f7f5519426e2831560ff80c60aef0d286734ce
jheart-vic <adebowalevictorjheart.com>
2022-08-08 20:48:12 +0100

    Made changes reqiured by reviewer

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-SELECT name from animals WHERE date_of_birth BETWEEN 'January 1, 2016' AND 'January 1, 2019';
+SELECT name from animals WHERE date_of_birth BETWEEN 'January 1, 2016' AND 'December 31, 2019';
 SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
